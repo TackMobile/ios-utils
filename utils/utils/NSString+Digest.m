d57eb2eeee45201adabe4ccdae91962ca5877ffc
Tony Hillerson <tony.hillerson@gmail.com>
2012-07-26 23:19:45 -0600

    Cargo culting this a different way

--- utils/utils/NSString+Digest.m
+++ utils/utils/NSString+Digest.m
@@ -12,12 +12,19 @@
 @implementation NSString (SHA1)
 
 - (NSString *) sha1 {
-    unsigned char digest[CC_SHA1_DIGEST_LENGTH];
-    NSData *stringBytes = [self dataUsingEncoding:NSUTF8StringEncoding];
-    if (CC_SHA1([stringBytes bytes], [stringBytes length], digest)) {
-        return [[NSString alloc] initWithBytes:digest length:sizeof(digest) encoding:NSASCIIStringEncoding];
-    }
-    return nil;
+    const char *cstr = [self cStringUsingEncoding:NSUTF8StringEncoding];
+    NSData *data = [NSData dataWithBytes:cstr length:self.length];
+
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+
+    CC_SHA1(data.bytes, data.length, digest);
+
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+    [output appendFormat:@"%02x", digest[i]];
+
+    return output;
 }
 
 @end
